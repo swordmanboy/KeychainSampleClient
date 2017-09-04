2ec68b5a79f3482bc8608ac172720fb56064bd91
Apinun Wongintawang <apinun.wong@gmail.com>
2017-09-04 14:00:43 +0700

    first commit

---
+++ KeychainSampleClient/Pods/Target Support Files/Pods-KeychainSampleClient/Pods-KeychainSampleClient-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_KeychainSampleClient : NSObject
+@end
+@implementation PodsDummy_Pods_KeychainSampleClient
+@end
---
+++ KeychainSampleClient/Pods/Target Support Files/Pods-KeychainSampleClientTests/Pods-KeychainSampleClientTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_KeychainSampleClientTests : NSObject
+@end
+@implementation PodsDummy_Pods_KeychainSampleClientTests
+@end
